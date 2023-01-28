90688f8f547c734b5aefdbf3436452a4e40d0c7e
sseget <s.seget@avsystem.com>
2023-01-28 19:23:45 +0100

    fix

--- app/server.py
+++ app/server.py
@@ -12,9 +12,10 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 class DataPoint:
-    def __init__(self, date, value):
+    def __init__(self, date, value, error):
         self.date = date
         self.value = value
+        self.error = error
     
     def toJSON(self):
         return self.__dict__
@@ -26,14 +27,14 @@ def hello():
     date_str = request.args.get('date')
     date_object = datetime.strptime(date_str, "%Y-%m-%d")
     delta = timedelta(days=1)
-    arr = [DataPoint(date_object.strftime("%Y-%m-%d"), 12), 
-    DataPoint((date_object + delta).strftime("%Y-%m-%d"), 2), 
-    DataPoint((date_object + delta * 2).strftime("%Y-%m-%d"), 4), 
-    DataPoint((date_object + delta * 3).strftime("%Y-%m-%d"), 15),
-    DataPoint((date_object + delta * 4).strftime("%Y-%m-%d"), 7),
-    DataPoint((date_object + delta * 5).strftime("%Y-%m-%d"), 18),
-    DataPoint((date_object + delta * 6).strftime("%Y-%m-%d"), 38),
-    DataPoint((date_object + delta * 7).strftime("%Y-%m-%d"), 1)]
+    arr = [DataPoint(date_object.strftime("%Y-%m-%d"), 12, 1), 
+    DataPoint((date_object + delta).strftime("%Y-%m-%d"), 2, 1), 
+    DataPoint((date_object + delta * 2).strftime("%Y-%m-%d"), 4, 2), 
+    DataPoint((date_object + delta * 3).strftime("%Y-%m-%d"), 15, 3),
+    DataPoint((date_object + delta * 4).strftime("%Y-%m-%d"), 7, 3),
+    DataPoint((date_object + delta * 5).strftime("%Y-%m-%d"), 18, 4),
+    DataPoint((date_object + delta * 6).strftime("%Y-%m-%d"), 38, 4),
+    DataPoint((date_object + delta * 7).strftime("%Y-%m-%d"), 10, 4)]
     data_points_json = [dp.toJSON() for dp in arr]
     return jsonify(data_points_json)
 
