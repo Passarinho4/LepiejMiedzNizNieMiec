70b259aec44c132eb29ce0aa774f05fadece5d37
mnowak <m.nowak@avsystem.com>
2023-01-28 20:03:45 +0100

    model and api binding

--- app/server.py
+++ app/server.py
@@ -4,7 +4,6 @@ from flask import request
 from datetime import datetime, timedelta
 from flask import jsonify
 from predictor import Predictor
-import json
 
 
 app = Flask(__name__)
@@ -18,15 +17,15 @@ class DataPoint:
     def __init__(self, date, value, error):
         self.date = date
         self.value = value
-self.error = error
+        self.error = error
 
     def toJSON(self):
         return self.__dict__
 
 
-@app.route("/dupa")
+@app.route("/predict")
 @cross_origin()
-def hello():
+def predict():
     date_str = request.args.get('date')
     date_object = datetime.strptime(date_str, "%Y-%m-%d")
 
@@ -35,6 +34,7 @@ def hello():
     arr = [DataPoint((date_object + delta * (n + 1)).strftime("%Y-%m-%d"), predicted[n], 1) for n in range(7)]
     data_points_json = [dp.toJSON() for dp in arr]
     return jsonify(data_points_json)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=4000)
